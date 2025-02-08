4c31ce38a06ef55ebaf6ac82c97634fe5041e15b
Roberto <robercantalejo@gmail.com>
2025-02-08 21:24:58 +0000

    Se pone el botón para cerrar y se hacen ediciones estéticas

--- src/js/components/ToDoList.jsx
+++ src/js/components/ToDoList.jsx
@@ -2,16 +2,22 @@ import React, {useState} from 'react'
 
 const ToDoList = () => {
 
+  // Creo un useState para la lista y otro para ir metiendo cosas
+  
   const [list, setList] = useState([])
   const [thing, setThing] = useState("")
 
   const addToList = (eventInfo) => {
-    if (eventInfo.key === 'Enter' && thing.trim !== '') {
+    if (eventInfo.key === 'Enter' && thing.trim() !== '') {
       setList([...list, thing]);
       setThing('')
     }
-    
   }
+
+  const deleteThing = (index) => {
+    const newList = list.filter((_, i) => i !== index); 
+    setList(newList);
+};
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center vh-100">
@@ -22,13 +28,18 @@ const ToDoList = () => {
         <input type="text" className="form-control" value={thing} onChange={(eventInfo) => setThing(eventInfo.target.value)} onKeyDown={addToList} />
       </div>
 
-      <div className="mt-3" style={{width:'400px'}}>
-      <h4>Please Future Me, don't forget about:</h4>
-      <ul class="list-group" style={{width:'400px', display:'flex', justifyContent:'center'}}>
-        {list.map((task) =>
-          <li class="list-group-item list-group-item-primary small">{task}</li>
+      <div className="mt-3 list-container">
+      <div className="list-header d-flex justify-content-center px-4 pb-1 pt-1 border-bottom border-primary">
+      <h6 id="list-reminder">Please Future Me, don't forget about...</h6>
+      </div>
+      <ul className="list-group">
+        {list.map((task, index) =>
+          <li className="list-group-item list-group-item-light small rounded-right-top rounded-0 px-3 py-1">{task}
+          <button type="button" class="btn-close" aria-label="Close" onClick={() => deleteThing(index)}></button>
+          </li>
         )}
       </ul>
+      <div className='list-footer d-flex justify-content-start px-2 pb-1 pt-1'>{list.length} items left</div>
     </div>
   </div>
   );
